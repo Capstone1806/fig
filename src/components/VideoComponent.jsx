d4f8a26d64bdc21fdbb4e8740ce3f01f9a0825aa
Synuhe <synuhe.uribe@gmail.com>
2018-08-19 18:06:15 -0400

    fixed linter bugs

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -1,63 +1,12 @@
 import React, {Component} from 'react'
 import Video from 'twilio-video'
 import axios from 'axios'
-import PropTypes from 'prop-types'
-import {withStyles} from '@material-ui/core/styles'
 import FormControl from '@material-ui/core/FormControl'
-import purple from '@material-ui/core/colors/purple'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 // import RaisedButton from 'material-ui/RaisedButton'
 // import TextField from 'material-ui/TextField'
 // import {Card, CardHeader, CardText} from 'material-ui/Card'
-
-const styles = theme => ({
-  container: {
-    flexWrap: 'wrap',
-    textAlign: 'center',
-    position: 'relative',
-    display: 'block',
-    width: '100%'
-  },
-  margin: {
-    margin: theme.spacing.unit
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200
-  },
-  cssLabel: {
-    '&$cssFocused': {
-      color: purple[500]
-    }
-  },
-  cssFocused: {},
-  cssUnderline: {
-    '&:after': {
-      borderBottomColor: purple[500]
-    }
-  },
-  bootstrapRoot: {
-    padding: 0,
-    'label + &': {
-      marginTop: theme.spacing.unit * 3
-    }
-  },
-  bootstrapInput: {
-    borderRadius: 4,
-    backgroundColor: theme.palette.common.white,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    padding: '10px 12px',
-    width: 'calc(100% - 24px)',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    '&:focus': {
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
-    }
-  }
-})
 
 export default class VideoComponent extends Component {
   constructor(props) {
@@ -157,7 +106,6 @@ export default class VideoComponent extends Component {
       console.log("Already in Room: '" + participant.identity + "'")
       var previewContainer = this.refs.remoteMedia
       this.attachParticipantTracks(participant, previewContainer)
-      previewContainer.children[1].setAttribute('width', '100%')
     })
 
     // When a Participant joins the Room, log the event.
@@ -196,7 +144,8 @@ export default class VideoComponent extends Component {
       }
       this.detachParticipantTracks(room.localParticipant)
       room.participants.forEach(this.detachParticipantTracks)
-      this.state.activeRoom = null
+      this.setState({activeRoom: null})
+      // this.state.activeRoom = null
       this.setState({hasJoinedRoom: false, localMediaAvailable: false})
     })
   }
